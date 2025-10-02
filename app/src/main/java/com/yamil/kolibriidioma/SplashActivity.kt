0766a330026f331166ea04d86f793dd3ffc7eb1b
Santiago Yamil <santiagoyamil2005@gmail.com>
2025-10-02 13:38:20 -0600

    Ya jala

--- app/src/main/java/com/yamil/kolibriidioma/SplashActivity.kt
+++ app/src/main/java/com/yamil/kolibriidioma/SplashActivity.kt
@@ -51,7 +51,7 @@ class SplashActivity : AppCompatActivity() {
                 startActivity(Intent(this, SelectionLanguageActivity::class.java))
             } else {
                 // Ya hay idioma guardado → ir directo a la pantalla principal
-                startActivity(Intent(this, DetailProductActivity::class.java))
+                startActivity(Intent(this, MainActivityDrawer::class.java))
             }
 
             finish() // cerrar esta actividad
